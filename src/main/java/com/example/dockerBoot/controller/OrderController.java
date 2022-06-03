a8ce0aa43cda2abd50d7730f770196ac13e97f90
逐风 <973308067@qq.com>
2022-06-03 10:14:27 +0800

    Changes

--- src/main/java/com/example/dockerBoot/controller/OrderController.java
+++ src/main/java/com/example/dockerBoot/controller/OrderController.java
@@ -16,7 +16,7 @@ public class OrderController
     @RequestMapping("/order/docker")
     public String helloDocker()
     {
-        return "hello docker"+"\t"+port+"\t"+ UUID.randomUUID().toString() + "这是修改999";
+        return "hello docker"+"\t"+port+"\t"+ UUID.randomUUID().toString() + "这是修改1010";
     }
 
     @RequestMapping(value ="/order/index",method = RequestMethod.GET)
